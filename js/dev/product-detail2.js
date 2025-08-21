0ca9958f4f8ee5c53d9a49d831dedf73df619352
ValeriiaZhubrieva <67803539+ValeriiaZhubrieva@users.noreply.github.com>
2025-08-21 17:20:44 +0300

    fix v3

--- js/dev/product-detail2.js
+++ js/dev/product-detail2.js
@@ -1,19 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const totalElement = document.querySelector(".fixed-block-hide");
+  const totalElements = document.querySelectorAll(".fixed-block-hide");
   const fixedElement = document.querySelector(".fixed-block");
-  if (!totalElement || !fixedElement) return;
+  if (!totalElements.length || !fixedElement) return;
   const observer = new IntersectionObserver(
     (entries) => {
+      let anyVisible = false;
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          fixedElement.classList.add("hide");
-        } else {
-          fixedElement.classList.remove("hide");
+          anyVisible = true;
         }
       });
+      if (anyVisible) {
+        fixedElement.classList.add("hide");
+      } else {
+        fixedElement.classList.remove("hide");
+      }
     },
     { threshold: 0.1 }
-    // Элемент считается видимым, если хотя бы 10% его высоты видны
   );
-  observer.observe(totalElement);
+  totalElements.forEach((el) => observer.observe(el));
 });
